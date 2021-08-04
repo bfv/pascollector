7f8d707b4ec5276c8d7d5b146079c11d6c35090f
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2021-08-04 10:38:52 +0200

    first draft collecting data

--- cmd/server.go
+++ cmd/server.go
@@ -16,12 +16,18 @@ limitations under the License.
 package cmd
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 	"sync"
 	"time"
 
+	"github.com/Jeffail/gabs/v2"
+	"github.com/bfv/pascollector/types"
+	"github.com/shirou/gopsutil/process"
 	"github.com/spf13/cobra"
 )
 
@@ -53,6 +59,15 @@ var serverStopCmd = &cobra.Command{
 	},
 }
 
+var serverListCms = &cobra.Command{
+	Use:   "list",
+	Short: "List servers",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		listServers()
+	},
+}
+
 func init() {
 
 	serverCmd.AddCommand(serverStartCmd)
@@ -62,10 +77,9 @@ func init() {
 
 }
 
-func runServerCommand(cmd *cobra.Command, args []string) {
-
-	for _, server := range Config.Servers {
-		fmt.Println(server.Name + ": " + server.Url)
+func listServers() {
+	for _, instance := range Config.PasInstances {
+		fmt.Println(instance.Name + ": " + instance.Url)
 	}
 }
 
@@ -86,14 +100,22 @@ func startServer() {
 
 func stopServer() {
 	fmt.Println("stopping server...")
-	http.Get("http://localhost:" + strconv.Itoa(Config.Port) + "/stop")
+	res, err := http.Get("http://localhost:" + strconv.Itoa(Config.Port) + "/stop")
+	if err == nil {
+		defer res.Body.Close()
+		body, _ := io.ReadAll(res.Body)
+		fmt.Println(string(body))
+	} else {
+		fmt.Println(err)
+	}
 }
 
 func startListener() {
 
 	http.HandleFunc("/stop", func(w http.ResponseWriter, r *http.Request) {
+		defer wg.Done()
 		fmt.Println("stop request received")
-		wg.Done()
+		fmt.Fprintf(w, "OK")
 	})
 
 	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
@@ -114,7 +136,25 @@ func dataCollector() {
 }
 
 func collectData() {
-	fmt.Println("collect: " + time.Now().String())
+
+	var ablApps []types.ABLApplication
+	//fmt.Println("collect: " + time.Now().String())
+
+	for _, instance := range Config.PasInstances {
+		ablApps = getAblApps(instance)
+
+		for idx, ablApp := range ablApps {
+			agents := getAgents(instance, ablApp)
+			ablApps[idx].Agents = agents
+
+			for idx, agent := range agents {
+				agentStats := getAgentStats(instance, ablApp, agent)
+				agents[idx].Stats = agentStats
+			}
+		}
+	}
+
+	fmt.Println(ablApps)
 }
 
 func dataSender() {
@@ -137,4 +177,93 @@ func displayConfig() {
 	fmt.Println("  port: " + strconv.Itoa(Config.Port))
 	fmt.Println("  collect interval: " + strconv.Itoa(Config.CollectInterval) + "s")
 	fmt.Println("  send interval: " + strconv.Itoa(Config.SendInterval) + "s")
+	fmt.Println("---")
+}
+
+func callGetOeManager(instance types.PasInstance, endpoint string) ([]byte, error) {
+
+	client := &http.Client{}
+	url := instance.Url + "/oemanager" + endpoint
+
+	auth := fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(instance.Creds)))
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("Authorization", auth) // "Basic YmZ2OmJmdg=="
+
+	res, err := client.Do(req)
+
+	if err == nil {
+		body, err := io.ReadAll(res.Body)
+		return body, err
+	}
+
+	return nil, err
+}
+
+func getAblApps(instance types.PasInstance) []types.ABLApplication {
+
+	ablApps := []types.ABLApplication{}
+
+	body, err := callGetOeManager(instance, "/applications")
+
+	if err == nil {
+		jsonParsed, _ := gabs.ParseJSON(body)
+
+		for _, child := range jsonParsed.Path("result.Application").Children() {
+			var app types.ABLApplication
+			json.Unmarshal(child.Bytes(), &app)
+
+			ablApps = append(ablApps, app)
+		}
+	}
+
+	return ablApps
+}
+
+func getAgents(instance types.PasInstance, ablApp types.ABLApplication) []types.MSAgent {
+
+	agents := []types.MSAgent{}
+
+	path := fmt.Sprintf("/applications/%s/agents", ablApp.Name)
+	body, err := callGetOeManager(instance, path)
+
+	if err == nil {
+		jsonParsed, _ := gabs.ParseJSON(body)
+
+		for _, child := range jsonParsed.Path("result.agents").Children() {
+
+			var agent types.MSAgent
+			json.Unmarshal(child.Bytes(), &agent)
+
+			agents = append(agents, agent)
+		}
+	}
+
+	return agents
+}
+
+func getAgentStats(instance types.PasInstance, ablApp types.ABLApplication, agent types.MSAgent) types.MSAgentStat {
+
+	var agentStat types.MSAgentStat
+
+	path := fmt.Sprintf("/applications/%s/agents/%s/metrics", ablApp.Name, agent.AgentId)
+	body, err := callGetOeManager(instance, path)
+
+	if err == nil {
+		jsonParsed, _ := gabs.ParseJSON(body)
+		if len(jsonParsed.Path("result.AgentStatHist").Children()) > 0 {
+			jsonBytes := jsonParsed.Path("result.AgentStatHist").Children()[0].Bytes()
+			json.Unmarshal(jsonBytes, &agentStat)
+
+			pid, _ := strconv.Atoi(agent.Pid)
+			proc, err := process.NewProcess(int32(pid))
+
+			if err == nil {
+				mem, _ := proc.MemoryInfo()
+				agentStat.MemRSS = mem.RSS
+				agentStat.MemVirtual = mem.VMS
+			}
+		}
+	}
+
+	return agentStat
 }
