467bcd2130255703d98c3bc378f760435be075c7
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2021-08-04 10:38:27 +0200

    added os specific code

---
+++ misc/os.go
@@ -0,0 +1,37 @@
+package misc
+
+// OS specific code goes here
+
+import (
+	"log"
+	"os"
+	"os/user"
+	"runtime"
+)
+
+func GetConfigDir() string {
+
+	var configDir string
+
+	switch osName := runtime.GOOS; osName {
+	case "windows":
+		configDir = os.Getenv("ProgramData") + "\\"
+	case "linux":
+		configDir = "/etc/"
+	}
+
+	configDir += "pascollector"
+
+	return configDir
+}
+
+func GetConfigurationFilename() string {
+	return GetConfigDir() + string(os.PathSeparator) + ".pascollector.yaml"
+}
+
+func CheckUser() {
+	currentUser, _ := user.Current()
+	if runtime.GOOS == "linux" && currentUser.Username != "root" {
+		log.Fatal("setup should be ran as root")
+	}
+}
